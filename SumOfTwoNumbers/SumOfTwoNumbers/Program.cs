ccddd63e9219f1427622143d8cdf574d7feac38c
VT003 <vg2165@srmist.edu.in>
2021-08-01 00:43:50 +0530

    Sum of two numbers

---
+++ SumOfTwoNumbers/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum is: " + (a + b));
+        }
+    }
+}
