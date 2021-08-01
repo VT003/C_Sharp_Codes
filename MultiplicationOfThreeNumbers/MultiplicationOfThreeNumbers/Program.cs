2695a6fb4c7e7a014f206827db8a2fb7791f05c3
VT003 <vg2165@srmist.edu.in>
2021-08-01 20:26:14 +0530

    Multiplication of three numbers.

---
+++ MultiplicationOfThreeNumbers/MultiplicationOfThreeNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplicationOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter first number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter first number: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Product: " + a * b * c);
+        }
+    }
+}
