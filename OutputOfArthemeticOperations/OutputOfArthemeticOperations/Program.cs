d3b82880ae39dce23049b071f8bdbb9b1ec7cb03
VT003 <vg2165@srmist.edu.in>
2021-08-01 20:31:26 +0530

    Output of adding, subtracting, multiplying, dividing and mod of two numbers.

---
+++ OutputOfArthemeticOperations/OutputOfArthemeticOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OutputOfArthemeticOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sum: " + (a + b));
+            Console.WriteLine("Difference: " + (a - b));
+            Console.WriteLine("Product: " + a * b);
+            Console.WriteLine("Quotient: " + a / b);
+            Console.WriteLine("Modulus: " + a % b);
+        }
+    }
+}
