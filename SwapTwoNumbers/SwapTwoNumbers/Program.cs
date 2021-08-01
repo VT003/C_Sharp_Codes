9eba93052e5c5660b670b3b4bdd30343b2389e38
VT003 <vg2165@srmist.edu.in>
2021-08-01 20:23:29 +0530

    Swapping two numbers.

---
+++ SwapTwoNumbers/SwapTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swapping");
+            Console.WriteLine("First Number: " + a);
+            Console.WriteLine("Second Number: " + b);
+        }
+    }
+}
