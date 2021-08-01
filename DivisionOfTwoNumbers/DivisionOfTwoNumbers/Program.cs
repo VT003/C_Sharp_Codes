2f272e30faeaf74bbb7b260c367a933892802693
VT003 <vg2165@srmist.edu.in>
2021-08-01 20:12:24 +0530

    Division of two numbers.

---
+++ DivisionOfTwoNumbers/DivisionOfTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DivisionOfTwoNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// Division of two numbers.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("The Quotient is: " + a / b);
+            Console.WriteLine("The Remainder is: " + a % b);
+        }
+    }
+}
