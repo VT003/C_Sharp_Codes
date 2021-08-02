750d479d087484859669cd5dc7d892361b66e193
VT003 <vg2165@srmist.edu.in>
2021-08-03 00:32:16 +0530

    Enter your age for suprise.

---
+++ EnterYourAge/EnterYourAge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnterYourAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("You look older than " + age);
+        }
+    }
+}
