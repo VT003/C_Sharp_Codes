55b5463a0fa747850ddd5b69c146d2e05f6aef64
VT003 <vg2165@srmist.edu.in>
2021-08-03 00:02:13 +0530

    Printing multiplication table of user input number.

---
+++ PrintMultiplicationTable/PrintMultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintMultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Multiplication table of " + a + " is");
+            int i = 1;
+            while(i <= 10)
+            {
+                Console.WriteLine(a + " x " + i + " = " + a * i);
+                i++;
+            }
+        }
+    }
+}
