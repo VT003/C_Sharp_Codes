85f3e6fc0b78f3d14caf001c599fb56c179f78e2
VT003 <vg2165@srmist.edu.in>
2021-08-03 00:15:36 +0530

    Average of four given user numbers.

---
+++ FourNumberAverage/FourNumberAverage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FourNumberAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Enter four number: ");
+            int d = int.Parse(Console.ReadLine());
+            float avg = (a + b + c + d) / 4;
+            Console.WriteLine("Average of given four number: " + avg);
+
+        }
+    }
+}
