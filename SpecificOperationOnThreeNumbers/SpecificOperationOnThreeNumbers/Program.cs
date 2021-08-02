23f1f837847bc403df7b623a078be3ccf9588eef
VT003 <vg2165@srmist.edu.in>
2021-08-03 00:29:01 +0530

    Operation of (x + y) * z and x * y + y * z on given input.

---
+++ SpecificOperationOnThreeNumbers/SpecificOperationOnThreeNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpecificOperationOnThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Enter z: ");
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Output of (x + y) * z: " + (x + y) * z);
+            Console.WriteLine("Output of (x * y) + (y * z): " + ((x + y) * (y * z)));
+        }
+    }
+}
